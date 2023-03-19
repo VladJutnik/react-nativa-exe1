f3709323ce1acf61d891c8300232fce203a8abfc
VA <v2517v@gmail.com>
2023-03-19 20:33:43 +0700

    working with the main page and components

--- app/index.js
+++ app/index.js
@@ -1,22 +1,6 @@
-import { View, Text } from "react-native";
-import { Link, Stack } from "expo-router";
 
-//ссылки на все элементы приложения
-export default function Home() {
-    return (
-        <View>
-            <Text>Первый текст в мобильном приложении</Text>
-        </View>
-    )
+import { Redirect } from "expo-router";
+
+export default function Index() {
+    return <Redirect href="/home" />;
 }
-/*
-export default function Home() {
-    return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            {/!* Use the `Screen` component to configure the layout. *!/}
-            <Stack.Screen options={{ title: "Overview" }} />
-            {/!* Use the `Link` component to enable optimized client-side routing. *!/}
-            <Link href="/details">Go to Details</Link>
-        </View>
-    );
-}*/

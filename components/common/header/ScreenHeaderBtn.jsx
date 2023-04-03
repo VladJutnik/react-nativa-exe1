201277f1b3c1f993fb218622393efc1eaa40b555
VA <v2517v@gmail.com>
2023-04-03 20:49:05 +0700

    ready-made applications

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -3,15 +3,15 @@ import { Image, TouchableOpacity } from "react-native";
 import styles from "./screenheader.style";
 
 const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
-    return (
-        <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
-            <Image
-                source={iconUrl}
-                resizeMode='cover'
-                style={styles.btnImg(dimension)}
-            />
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode='cover'
+        style={styles.btnImg(dimension)}
+      />
+    </TouchableOpacity>
+  );
 };
 
 export default ScreenHeaderBtn;
